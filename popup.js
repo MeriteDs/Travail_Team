3cff2a38362c14c935d8aaa3c430989cbc96d7c0
MeriteDs <mufungizimerite45@gmail.com>
2024-08-05 15:06:32 +0200

    le travail

---
+++ popup.js
@@ -0,0 +1,26 @@
+let popup = document.getElementById("popup");
+
+function ouvrirPopup(){
+    popup.classList.add("ouvrir-popup");
+}
+function fermePopup(){
+    popup.classList.remove("ouvrir-popup");
+}
+
+let menu = document.getElementById("menu");
+
+function ouvrirMenu(){
+    menu.classList.add("ouvrir-menu");
+}
+function fermerMenu(){
+    menu.classList.remove("ouvrir-menu");
+}
+
+let dropmenu = document.getElementById("DropMenu");
+
+function ouvrirDropMenu(){
+    dropmenu.classList.add("ouvrir-dropMenu");
+}
+function fermerDropMenu(){
+    dropmenu.classList.remove("ouvrir-dropMenu");
+}
